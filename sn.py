3457e923378e5f60a3c456099f390713f9f21689
Chen Meng <observerchan@gmail.com>
2022-04-29 07:41:24 +0800

    .

--- sn.py
+++ sn.py
@@ -7,9 +7,28 @@ import xl
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 xmlp5_dir = os.path.join(PROJECT_ROOT, "xml-p5")
 
-xmlstr = open(os.path.join(xmlp5_dir, "N/N13/N13n0006.xml"), "r").read()
+xmls = [
+    "N/N13/N13n0006.xml",
+    "N/N14/N14n0006.xml",
+    "N/N15/N15n0006.xml",
 
-xml = xl.parse(xmlstr, do_strip=True)
-xmlstr2 = xml.to_str(do_pretty=True)
+    "N/N16/N16n0006.xml",
+    "N/N17/N17n0006.xml",
 
-open(os.path.join(PROJECT_ROOT, "1.xml"), "w").write(xmlstr2)
+    "N/N18/N18n0006.xml",
+    "N/N19/N19n0006.xml"
+]
+
+
+for one in xmls:
+    xmlstr = open(os.path.join(xmlp5_dir, one), "r").read()
+
+    xml = xl.parse(xmlstr, do_strip=True)
+
+    tei = xml.root
+
+    text = tei.find_kids("text")[0]
+    body = text.find_kids("body")[0]
+    cb_div1 = body.find_kids("cb:div")[0]
+    p = cb_div1.find_kids("p")[0]
+    print(p.kids)
