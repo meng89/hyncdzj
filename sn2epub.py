7c9d69141bcd2866212c722a2f48fda472435ad3
Chen Meng <observerchan@gmail.com>
2022-08-28 23:15:00 +0800

    .

--- sn2epub.py
+++ sn2epub.py
@@ -6,8 +6,34 @@ import epub_public
 import sn
 
 
+class TermsType:
+    pin = "pin"
+    sutta = "sutta"
+
+
+def pin_or_sutta(terms):
+    pin_count = 0
+    sutta_count = 0
+    for term in terms:
+        if isinstance(term, sn.Container):
+            if term.mulu[-1] == "品":
+                pin_count += 1
+            elif term.mulu[-1] == ""
+
+
+
+
 def write_suttas(nikaya, epub: epubpacker.Epub, bns, xc, _test=False):
     for pian in nikaya.terms:
+        for xy in pian.terms:
+            match pin_or_sutta(xy.terms):
+                case TermsType.pin:
+                    pass
+                case TermsType.sutta:
+                    pass
+
+
+
 
 
 
