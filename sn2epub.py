dae3df53532a4f993bd509e990fa3849aff1c95b
Chen Meng <observerchan@gmail.com>
2022-10-21 17:48:33 +0800

    .

--- sn2epub.py
+++ sn2epub.py
@@ -8,6 +8,21 @@ import epub_public
 import sn
 
 
+def no_serial_titlex(mulu: str):
+    # 第一　葦品
+    m = re.match("第[一二三四五六七八九十]+　(.+)$", mulu)
+    if m:
+        return m.group(1)
+
+    # 第一品
+    # 異學廣說
+    m = re.match("^(\S)$", mulu)
+    if m:
+        return m.group(1)
+
+    raise Exception
+
+
 def term2xml(term):
     return term.to_xml()
 
@@ -58,8 +73,50 @@ def _xy_name(xy: sn.Container):
     m = re.match(r"^第[一二三四五六七八九十]+　(.+相應)$", xy.mulu)
     return m.group(0)
 
+def transform_digit(han_digit: str):
+    str_digit = han_digit\
+        .replace("〇", "0")\
+        .replace("一", "1")\
+        .replace("二", "2")\
+        .replace("三", "3")\
+        .replace("四", "4")\
+        .replace("五", "5")\
+        .replace("六", "6")\
+        .replace("七", "7")\
+        .replace("八", "8")\
+        .replace("九", "9")
+    return int(str_digit)
 
-def _sutta_range(container: sn.Container):
+
+def get_sutta_range_and_name(mulu):
+    m = re.match("〔([一二三四五六七八九〇])+〕(\S*)$", mulu)
+    if m:
+        serial = transform_digit(m.group(1))
+        return serial, serial, m.group(2)
+
+    m = re.match("〔([一二三四五六七八九〇])+[、|～]([一二三四五六七八九〇])+〕(\S*)$", mulu)
+    if m:
+        return transform_digit(m.group(1)), transform_digit(m.group(2)), m.group(3)
+
+    raise Exception
+
+
+def get_sutta_begin(container:sn.Container):
+    if sn.is_sutta(container):
+        return get_sutta_range_and_name(container.terms[0])[0]
+    else:
+        return get_sutta_begin(container.terms[0])
+
+def get_sutta_end(container:sn.Container):
+    if sn.is_sutta(container):
+        return get_sutta_range_and_name(container.terms[-1])[0]
+    else:
+        return get_sutta_end(container.terms[0])
+
+def get_sutta_range(container:sn.Container):
+    return get_sutta_begin(container), get_sutta_end(container)
+
+
 
 
 
@@ -104,22 +161,42 @@ def write_suttas(nikaya: sn.SN, ebook: epubpacker.Epub, bns, xc, _test=False):
             if sn.is_sutta(xy):
                 write_sutta(xy)
             else:
-                write_before_sutta(xy, ebook, xc, body)
+                write_before_sutta(nikaya, xy, doc_path, ebook, xy_toc, xc, body)
 
 
-def write_before_sutta(nikaya: sn.SN, container: sn.Container, ebook: epubpacker.Epub, xc, body: xl.Element):
+def write_before_sutta(nikaya: sn.SN, container: sn.Container,
+                       doc_path, ebook: epubpacker.Epub, toc,
+                       xc, body: xl.Element):
+    c = xc.c
     for term in container.terms:
+        name = no_serial_titlex
         _html_id = html_id(nikaya, term)
-        title =
-        xl.sub(body, "h3", {"class": "title", "id": _html_id}, kids=[c(pin.title)])
+        xl.sub(body, "h3", {"class": "title", "id": _html_id}, kids=[c(name)])
+        sutta_begin, sutta_end = get_sutta_range(term)
+        toc_name = "{} {}~{}".format(name, sutta_begin, sutta_end)
+        toc.kids.append(epubpacker.Toc(toc_name, doc_path + "#" + _html_id))
+
+        if sn.is_sutta(term):
+            write_sutta(nikaya, term, doc_path, ebook, toc, xc, body)
+        else:
+            write_before_sutta(nikaya, term, doc_path, ebook, toc, xc, body)
 
 
-def write_sutta(container):
-    # todo
+def write_sutta(nikaya, container, doc_path, ebook, toc, xc, body):
+    c = xc.c
+    for sutta in container.terms:
+        sutta_begin, sutta_end, name = get_sutta_range_and_name(sutta.mulu)
+        xl.sub(body, "H4", {"class": "title", "id": html_id(sutta, nikaya)}, kids=[c(name)])
 
+        for x in sutta.terms:
+            if isinstance(x, sn.Container):
+                write_after_sutta(nikaya, container, doc_path, ebook, toc, xc, body)
+            else:
+                body.kids.extend(term2xml(x))
 
-def write_after_sutta():
-    #todo
+def write_after_sutta(nikaya, container, doc_path, ebook, toc, xc, body):
+    body.sub("")
+    body.kids.append(xl.Element())container.mulu
 
 
 def make(xc, temprootdir, books_dir, epubcheck):
