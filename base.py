a103533a5545838852e705e7b334dd02f726f98d
Chen Meng <observerchan@gmail.com>
2024-06-23 22:17:39 +0800

    .

--- base.py
+++ base.py
@@ -3,7 +3,6 @@ import sys
 
 sys.path.append("/mnt/data/projects/xl")
 
-
 import abc
 import datetime
 import re
@@ -21,7 +20,7 @@ class Book(object):
     def __init__(self, path=None):
         if path:
             xmlstr = open(os.path.join(path, "sn.xml")).read()
-            self._xml = xl.parse(xmlstr, do_strip=True)
+            self._xml = xl.parse(xmlstr)
             self._entries = dir2entries(os.path.join(path, "entries"))
         else:
             self._xml = xl.Xml()
@@ -60,7 +59,7 @@ class Book(object):
 
 class _Artcle(object):
     def _make_new_xml(self):
-        self._xml = xl.Xml(xl.Element("root"))
+        self._xml = xl.Xml(root=xl.Element("root"))
         self._xml.root.kids.append(xl.Element("body"))
         self._xml.root.kids.append(xl.Element("notes"))
         self._xml.root.kids.append(xl.Element("ps"))
@@ -106,14 +105,14 @@ class Artcle(_Artcle):
                 self._title = m.group(3)
 
                 xmlstr = open(filepath).read()
-                self._xml = xl.parse(xmlstr, do_strip=True)
+                self._xml = xl.parse(xmlstr)
 
                 # 非SN 1.1 这样的经文，可能是礼敬偈子，或串联
             else:
                 self._is_piece = True
 
                 xmlstr = open(filepath).read()
-                self._xml = xl.parse(xmlstr, do_strip=True)
+                self._xml = xl.parse(xmlstr)
         else:
             self._book_abbr = book_abbr
             self._serial = serial
@@ -137,7 +136,7 @@ class Piece(_Artcle):
             if m:
                 self._serial = m.group(1)
                 xmlstr = open(filepath).read()
-                self._xml = xl.parse(xmlstr, do_strip=True)
+                self._xml = xl.parse(xmlstr)
             else:
                 raise Exception("无法解析文件名")
         else:
@@ -204,7 +203,7 @@ def load_from_xmlp5(xmls):
 
         xmlstr = file.read()
         file.close()
-        xml = xl.parse(xmlstr, do_strip=True)
+        xml = xl.parse(xmlstr)
         tei = xml.root
         text = tei.find_kids("text")[0]
         body = text.find_kids("body")[0]
@@ -311,7 +310,7 @@ def make_tree(book, cb_div: xl.Element):
                 print("debug")
                 print(type(kid))
             else:
-                new_elements, new_notes, note_index = xmlp5_to_simplexml.trans_element(kid, note_index)
+                new_elements, new_notes, note_index = xmlp5a_to_simplexml.trans_element(kid, note_index)
                 notes.extend(new_notes)
                 node.body.kids.extend(new_elements)
                 node.notes.kids.extend(notes)
@@ -400,170 +399,6 @@ def filter_(term: xl.Element or str):
     return new_e
 
 
-def eliminate_cbdiv(elements) -> list:
-    new_elements = []
-    for x in elements:
-        if isinstance(x, xl.Element) and x.tag == "cb:div":
-            new_elements.extend(eliminate_cbdiv(x.kids))
-        else:
-            new_elements.append(x)
-    return new_elements
-
-
-def check2(path: list, cb_div: xl.Element):
-    # print(path)
-    bit_map = []
-    for x in cb_div.kids:
-        if isinstance(x, xl.Element) and x.tag == "cb:div":
-            sub_path = path[:]
-            sub_path.append(x)
-            check2(sub_path, x)
-            bit_map.append((0, x))
-        # metadata
-        elif isinstance(x, xl.Element) and x.tag in ("cb:juan", "byline", "cb:mulu", "note"):
-            pass
-        else:
-            bit_map.append((1, x))
-
-    (have_head, head), (have_middle, middle), (have_tail, tail) = xxx(bit_map)
-
-    if have_middle:
-        print_path(path)
-        if have_middle:
-            print("  middles:")
-            for x in middle:
-                if isinstance(x, xl.Element):
-                    print("      ", x.to_str())
-                else:
-                    print("      ", x)
-        print()
-
-
-def print_path(path: list, step=0):
-    if len(path) == 0:
-        return None
-
-    cb_div = path.pop(0)
-    for x in cb_div.kids:
-        print(" " * step, end=""),
-        if isinstance(x, xl.Element) and x.tag in ("cb:mulu", "head"):
-            print(x.to_str())
-        elif isinstance(x, str):
-            print(x)
-        break
-
-    print_path(path, step + 4)
-
-
-def xxx(bit_map: list):
-    have_head = False
-    head = []
-
-    have_middle = False
-    middle = []
-
-    have_tail = False
-    tail = []
-
-    new_bit_map = bit_map[:]
-    while True:
-        if len(new_bit_map) > 0 and new_bit_map[0][0] == 1:
-            head.append(new_bit_map[0][1])
-            new_bit_map.pop(0)
-            have_head = True
-
-        else:
-            break
-
-    new_bit_map.reverse()
-    while True:
-        if len(new_bit_map) > 0 and new_bit_map[0][0] == 1:
-            tail.append(new_bit_map[0][1])
-            new_bit_map.pop(0)
-            have_tail = True
-        else:
-            break
-
-    new_bit_map.reverse()
-    for x in new_bit_map:
-        # print(x)
-        if x[0] == 1:
-            have_middle = True
-            middle.append(x[1])
-
-    return (have_head, head), (have_middle, middle), (have_tail, tail)
-
-
-def test(xmls):
-    # import xmlp5a_to_dir_sn
-    # xmls = xmlp5a_to_dir_sn.xmls
-
-    for one in xmls:
-        filename = os.path.join(config.xmlp5a_dir, one)
-        # print("\n"*2)
-        print("xml:", filename)
-        file = open(filename, "r")
-
-        xmlstr = file.read()
-        file.close()
-        xml = xl.parse(xmlstr, strip=True)
-        tei = xml.root
-        text = tei.find_kids("text")[0]
-        body = text.find_kids("body")[0]
-        body = filter_(body)
-        check2([], body)
-
-
-def test_xl(xmls):
-    import difflib
-
-    xmls2 = []
-    filter_xmls = ["B25n0144.xml", "D41n8904.xml", "N64n0031.xm", "T25n1509.xml"]
-    for xml in xmls:
-        in_it = False
-        for filter_xml in filter_xmls:
-            if filter_xml in xml:
-                in_it = True
-                break
-        if in_it:
-            pass
-        else:
-            xmls2.append(xml)
-
-    for one in xmls:
-
-        filename = os.path.join(config.xmlp5a_dir, one)
-        print("xml:", filename, end="")
-        file = open(filename, "r")
-        xmlstr = file.read()
-        file.close()
-
-        xml = xl.parse(xmlstr)
-        xmlstr2 = xml.to_str()
-
-        xmlstr_noblank = (xmlstr.replace(" ", "").replace("\t", "")
-                          .replace("\n", "").replace("\r", "").replace("　", ""))
-        xmlstr2_noblank = (xmlstr2.replace(" ", "").replace("\t", "")
-                           .replace("\n", "").replace("\r", "").replace("　", ""))
-
-        print(", check:", end="")
-        if xmlstr == xmlstr2:
-            print("succeed")
-        else:
-            print("fail")
-            f = open("/mnt/data/t1.xml", "w")
-            f.write(xmlstr)
-            f.close()
-
-            f = open("/mnt/data/t2.xml", "w")
-            f.write(xmlstr2)
-            f.close()
-
-            for x in difflib.context_diff(xmlstr_noblank, xmlstr2_noblank):
-                print(x)
-            input()
-
-
 def all_xmls(dir_=config.xmlp5a_dir):
     xmls = []
     for one in os.listdir(dir_, ):
@@ -584,12 +419,9 @@ def n_xmls():
     return xmls
 
 
-if __name__ == "__main__":
-    no_prefix_xmls = []
-    for one2 in sorted(all_xmls(config.xmlp5a_dir)):
-        if one2.startswith(config.xmlp5a_dir):
-            no_prefix_xmls.append(one2.removeprefix(config.xmlp5a_dir))
-        else:
-            raise Exception
+def main():
+    pass
 
-    test(n_xmls())
+
+if __name__ == "__main__":
+    pass
---
+++ check.py
@@ -0,0 +1,188 @@
+#!/usr/bin/env python3
+import os
+import sys
+
+sys.path.append("/mnt/data/projects/xl")
+
+import xl
+import config
+from base import all_xmls, n_xmls, filter_
+
+
+def eliminate_cbdiv(elements) -> list:
+    new_elements = []
+    for x in elements:
+        if isinstance(x, xl.Element) and x.tag == "cb:div":
+            new_elements.extend(eliminate_cbdiv(x.kids))
+        else:
+            new_elements.append(x)
+    return new_elements
+
+
+def check2(path: list, cb_div: xl.Element):
+    # print(path)
+    bit_map = []
+    for x in cb_div.kids:
+        if isinstance(x, xl.Element) and x.tag == "cb:div":
+            sub_path = path[:]
+            sub_path.append(x)
+            check2(sub_path, x)
+            bit_map.append((0, x))
+        # metadata
+        elif isinstance(x, xl.Element) and x.tag in ("cb:juan", "byline", "cb:mulu", "note"):
+            pass
+        else:
+            bit_map.append((1, x))
+
+    (have_head, head), (have_middle, middle), (have_tail, tail) = xxx(bit_map)
+
+    if have_middle:
+        print_path(path)
+        if have_middle:
+            print("  middles:")
+            for x in middle:
+                if isinstance(x, xl.Element):
+                    print("      ", x.to_str())
+                else:
+                    print("      ", x)
+        print()
+
+
+def print_path(path: list, step=0):
+    if len(path) == 0:
+        return None
+
+    cb_div = path.pop(0)
+    for x in cb_div.kids:
+        print(" " * step, end=""),
+        if isinstance(x, xl.Element) and x.tag in ("cb:mulu", "head"):
+            print(x.to_str())
+        elif isinstance(x, str):
+            print(x)
+        break
+
+    print_path(path, step + 4)
+
+
+def xxx(bit_map: list):
+    have_head = False
+    head = []
+
+    have_middle = False
+    middle = []
+
+    have_tail = False
+    tail = []
+
+    new_bit_map = bit_map[:]
+    while True:
+        if len(new_bit_map) > 0 and new_bit_map[0][0] == 1:
+            head.append(new_bit_map[0][1])
+            new_bit_map.pop(0)
+            have_head = True
+
+        else:
+            break
+
+    new_bit_map.reverse()
+    while True:
+        if len(new_bit_map) > 0 and new_bit_map[0][0] == 1:
+            tail.append(new_bit_map[0][1])
+            new_bit_map.pop(0)
+            have_tail = True
+        else:
+            break
+
+    new_bit_map.reverse()
+    for x in new_bit_map:
+        # print(x)
+        if x[0] == 1:
+            have_middle = True
+            middle.append(x[1])
+
+    return (have_head, head), (have_middle, middle), (have_tail, tail)
+
+
+def test(xmls):
+    # import xmlp5a_to_dir_sn
+    # xmls = xmlp5a_to_dir_sn.xmls
+
+    for one in xmls:
+        filename = os.path.join(config.xmlp5a_dir, one)
+        # print("\n"*2)
+        print("xml:", filename)
+        file = open(filename, "r")
+
+        xmlstr = file.read()
+        file.close()
+        xml = xl.parse(xmlstr, strip=True)
+        tei = xml.root
+        text = tei.find_kids("text")[0]
+        body = text.find_kids("body")[0]
+        body = filter_(body)
+        check2([], body)
+
+
+def test_xl(xmls):
+    import difflib
+
+    xmls2 = []
+    filter_xmls = ["B25n0144.xml", "D41n8904.xml", "N64n0031.xm", "T25n1509.xml"]
+    for xml in xmls:
+        in_it = False
+        for filter_xml in filter_xmls:
+            if filter_xml in xml:
+                in_it = True
+                break
+        if in_it:
+            pass
+        else:
+            xmls2.append(xml)
+
+    for one in xmls:
+
+        filename = os.path.join(config.xmlp5a_dir, one)
+        print("xml:", filename, end="")
+        file = open(filename, "r")
+        xmlstr = file.read()
+        file.close()
+
+        xml = xl.parse(xmlstr)
+        xmlstr2 = xml.to_str()
+
+        xmlstr_noblank = (xmlstr.replace(" ", "").replace("\t", "")
+                          .replace("\n", "").replace("\r", "").replace("　", ""))
+        xmlstr2_noblank = (xmlstr2.replace(" ", "").replace("\t", "")
+                           .replace("\n", "").replace("\r", "").replace("　", ""))
+
+        print(", check:", end="")
+        if xmlstr == xmlstr2:
+            print("succeed")
+        else:
+            print("fail")
+            f = open("/mnt/data/t1.xml", "w")
+            f.write(xmlstr)
+            f.close()
+
+            f = open("/mnt/data/t2.xml", "w")
+            f.write(xmlstr2)
+            f.close()
+
+            for x in difflib.context_diff(xmlstr_noblank, xmlstr2_noblank):
+                print(x)
+            input()
+
+
+def main():
+    no_prefix_xmls = []
+    for one2 in sorted(all_xmls(config.xmlp5a_dir)):
+        if one2.startswith(config.xmlp5a_dir):
+            no_prefix_xmls.append(one2.removeprefix(config.xmlp5a_dir))
+        else:
+            raise Exception
+    test(n_xmls())
+    test(all_xmls())
+
+
+if __name__ == "__main__":
+    main()
