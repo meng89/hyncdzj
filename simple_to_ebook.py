764fd3cbebaaef2668abf069154bc8d9c818141b
Chen Meng <observerchan@gmail.com>
2024-10-10 17:29:47 +0800

    .

--- simple_to_ebook.py
+++ simple_to_ebook.py
@@ -6,9 +6,12 @@ import tempfile
 
 import opencc
 
+import epubpacker
+
 
 import base
 import config
+from p5a_to_simple import write
 
 
 def load_from_dir(book_name):
@@ -47,13 +50,11 @@ def cover_to_sc(src, dst):
             cover_to_sc(path, sc_path)
 
 
-
 def write2pdf(book, book_name, module):
     write_tree(book, 1, f, module)
 
 
-def write_tree(d: base.Dir, level, f:io.TextIOWrapper, module):
-
+def write_tree(d: base.Dir, level, f: io.TextIOWrapper, module):
     for name, obj in d.list:
         bookmark_name = name
         if hasattr(module, "pdf_bookmark_name"):
@@ -62,7 +63,23 @@ def write_tree(d: base.Dir, level, f:io.TextIOWrapper, module):
         f.write("\\title{{}}{{}}".format(name, " "))
 
 
-
 def write2epub(book, module):
-    pass
+    epub = epubpacker.Epub()
 
+def write2epub2(d: base.Dir, level, epub, tocs, parent_toc, module):
+    for name, obj in d.list:
+        bookmark_name = name
+        if hasattr(module, "bookmark_name"):
+            bookmark_name = module.bookmark_name(name, obj, d)
+        toc = epubpacker.Toc(name)
+
+        if isinstance(obj, base.Doc):
+
+
+        elif isinstance(obj, base.Dir):
+            tocs.append(toc)
+            write2epub2(obj, level + 1, epub, tocs, toc, module)
+
+
+
+# 短小的合并之类操作应该修改dir对象来完成
