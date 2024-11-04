10260d56a1374e63670659a9add1d0b99addc601
Chen Meng <observerchan@gmail.com>
2024-11-04 17:29:04 +0800

    .

---
+++ dipavamsa.py
@@ -0,0 +1,5 @@
+import base
+# Dīpavamsa
+# 島王統史
+
+info = base.Info(32, "島王統史", ("悟醒", ), "MIL")
---
+++ kn_mil.py
@@ -0,0 +1,6 @@
+import base
+# Milindapañha
+# 彌蘭王問經
+
+
+info = base.Info(31, "彌蘭王問經", ("郭哲彰", ), "MIL")
